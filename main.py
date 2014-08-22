85117f4f45e8ab90e3a57d797cfe7f94a2345c75
barrand <barrand@gmail.com>
2014-08-22 01:12:19 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import sys
+sys.path.insert(0, 'libs')
+
+import webapp2
+import urllib2
+from bs4 import BeautifulSoup
+
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
+
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+		content = urllib2.urlopen('http://mar-numberfive.appspot.com/static/nycmqEpic.xml').read()
+		soup = BeautifulSoup(content)
+		print soup.hoteldescriptivecontent['hotelname']
+		self.response.write(soup.hoteldescriptivecontent['hotelname'])
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
