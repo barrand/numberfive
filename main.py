7abb89feeb8f9192c9236d6740492efd047a40f4
barrand <barrand@gmail.com>
2014-08-27 00:46:09 -0400

    added a ton of junk to the prototype. Pretty much ready for showoff tomorrow.

--- main.py
+++ main.py
@@ -39,8 +39,6 @@ PAGE_END_HTML = """\
   </body>
 </html>
 """
-description_en = ""
-description_br = ""
 
 class MainHandler(webapp2.RequestHandler):
 	
@@ -50,9 +48,55 @@ class MainHandler(webapp2.RequestHandler):
 		self.cityName = soup.find(contactprofiletype='Property Info').find('cityname').renderContents()
 		self.roomCount = soup.find_all('guestroominfo', code='8')[0]['quantity']
 		self.suiteCount = soup.find_all('guestroominfo', code='9')[0]['quantity']
-		print self.suiteCount
+		urbanTags = soup.find_all('locationcategory', code='3', existscode='1', codedetail="Location Type: City")
+		if len(urbanTags) > 0:
+			self.isUrban = True
+		else:
+			self.isUrban = False
+					
+		suburbanTags = soup.find_all('locationcategory', code='13', existscode='1', codedetail="Location Type: Suburban")
+		if len(suburbanTags) > 0:
+			self.isSuburban = True
+		else:
+			self.isSuburban = False
+		
+		freeBreakfastTag = soup.find('service', code='138', existscode='1')
+		if freeBreakfastTag is not None:
+			self.freeBreakfast = True
+		else:
+			self.freeBreakfast = False
+
+		print "free breakfast " + str(self.freeBreakfast)
+
+		# wifiTag = soup.find('businessservicecode', code='69', existscode='1')
+		# print "wifi " + wifiTag
+
+		meetingRoomTag = soup.find('meetingrooms')
+		self.meetingRooms = meetingRoomTag['meetingroomcount']
+		self.totalRoomSpace = meetingRoomTag['totalroomspace']
+		self.totalRoomSpaceUnit = meetingRoomTag['unitofmeasure']
+		print self.meetingRooms + self.totalRoomSpace + self.totalRoomSpaceUnit
+
+		self.airportName = soup.find('attraction', attractioncategorycode='1')['attractionname']
+		if self.airportName is not None:
+			print "airpport " + str(self.airportName)
+
+		# cityCenterTag = sout.find('attraction', attractioncategorycode='68')
+
+		self.musicPlace = soup.find('attraction', attractioncategorycode='71')['attractionname']
+		if self.musicPlace is not None:
+			print "music " + str(self.musicPlace)
+
+		self.attractions = []
+		attractionTag = soup.find_all('attraction', attractioncategorycode='18')
+		for t in attractionTag:
+			self.attractions.append(t['attractionname'])
+		print "attractions " + str(self.attractions)
+# self.airportName = soup.find('attraction', attractioncategorycode='1')['attractionname']
+# 		if self.airportName is not None:
+# 			print "airpport " + str(self.airportName)
+
 		# self.suiteCount = soup.
-		print self.cityName, self.hotelName
 
 	def buildDescriptions(self, soup):
 		self.currentLangs = ['en', 'br']
@@ -61,7 +105,12 @@ class MainHandler(webapp2.RequestHandler):
 			self.allDescriptions[lang] = ""
 		self.setupCommonVars(soup)
 		self.addCityJunk(soup)
+		self.addUrbanJunk(soup)
 		self.addRoomJunk(soup)
+		self.addMeetingsJunk(soup)
+		self.addAirportJunk(soup)
+		self.addMusicJunk(soup)
+		self.addAttractionJunk(soup)
 		# description = addStarJunk(description)
 		# description = addHistoricJunk(description)
 		# description = addGuestRoomsJunk(description)
@@ -84,6 +133,11 @@ class MainHandler(webapp2.RequestHandler):
 			tmpString = tmpString.replace('#h', self.hotelName);
 			self.allDescriptions[lang] += tmpString + " "
 
+	def addUrbanJunk(self, soup):
+		for lang in self.currentLangs:
+			tmpString = random.choice(sentences.urbanByLang[lang]).replace('#s', self.cityName);
+			self.allDescriptions[lang] += tmpString
+
 	def addRoomJunk(self, soup):
 		for lang in self.currentLangs:
 			#get a random city string by the language and replace the city name
@@ -91,10 +145,38 @@ class MainHandler(webapp2.RequestHandler):
 			tmpString = tmpString.replace('#s', self.suiteCount);
 			self.allDescriptions[lang] += tmpString
 
+	def addMeetingsJunk(self, soup):
+		for lang in self.currentLangs:
+			#get a random city string by the language and replace the city name
+			tmpString = random.choice(sentences.meetingByLang[lang]).replace('#n', self.meetingRooms);
+			tmpString = tmpString.replace('#s', self.totalRoomSpace);
+			tmpString = tmpString.replace('#u', self.totalRoomSpaceUnit);
+			self.allDescriptions[lang] += tmpString
+
+	def addAirportJunk(self, soup):
+		for lang in self.currentLangs:
+			#get a random city string by the language and replace the city name
+			tmpString = random.choice(sentences.airportByLang[lang]).replace('#a', self.airportName);
+			self.allDescriptions[lang] += tmpString
+
+	def addMusicJunk(self, soup):
+		for lang in self.currentLangs:
+			tmpString = random.choice(sentences.musicByLang[lang]).replace('#m', self.musicPlace);
+			self.allDescriptions[lang] += tmpString
+
+	def addAttractionJunk(self, soup):
+		for lang in self.currentLangs:
+			tmpString = random.choice(sentences.attractionsByLang[lang]).replace('#n', self.hotelName);
+			attList = ""
+			for a in self.attractions:
+				attList += a + ", "
+			tmpString = tmpString.replace('#a', attList)
+			self.allDescriptions[lang] += tmpString
+
+
 
 	def get(self):
 		self.response.write(PAGE_START_HTML)
-		self.response.write(sentences.description_en)
 		self.response.write(PAGE_END_HTML)
 	def post(self):
 		self.response.write(PAGE_START_HTML)
@@ -102,6 +184,7 @@ class MainHandler(webapp2.RequestHandler):
 		soup = BeautifulSoup(content)
 		self.buildDescriptions(soup)
 		for lang in self.currentLangs:
+			self.response.write('<br><b>' + lang + '</b><br>')
 			self.response.write(self.allDescriptions[lang])
 			self.response.write('<br><br>')
 		self.response.write(PAGE_END_HTML)
